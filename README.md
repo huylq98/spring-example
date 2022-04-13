7e9d0213cf3f96d7aeebc4b64d6851e659a04ba0
huylq33 <huylq33@viettel.com.vn>
2022-04-13 16:38:57 +0700

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # spring-example
+
+##1. Tech Stack
+* Frameworks: Spring Boot, Spring JPA.
+* Cache: Redis.
+* Database: H2.
+* Build tool: Maven.
+* Techniques:
+    * Using `@RestControllerAdvice` to handle exceptions globally.
+    * Using Mapstruct to convert between models.
+    * Using `Filter` and `@ControllerAdvice` to log requests and responses.
--- src/main/java/vn/com/huylq/springexample/SpringExampleApplication.java
+++ src/main/java/vn/com/huylq/springexample/SpringExampleApplication.java
@@ -1,20 +1,12 @@
 package vn.com.huylq.springexample;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import vn.com.huylq.springexample.service.UserService;
 
 @SpringBootApplication
 @EnableCaching
-@RequiredArgsConstructor
-@Slf4j
 public class SpringExampleApplication {
-
-    private final UserService userService;
-
     public static void main(String[] args) {
         SpringApplication.run(SpringExampleApplication.class, args);
     }
